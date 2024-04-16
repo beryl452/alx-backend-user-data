3c57b3c78e1e6f21f296ba63a00133e38872adf1
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-04-16 21:21:15 +0000

    End of 8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Basic authentication module for the API.
 """
-import re
-
+import base64
+import binascii
+from typing import Tuple, TypeVar
 from .auth import Auth
 
 
@@ -21,3 +22,19 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+   gdef decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+            ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
