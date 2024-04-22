5156e9b98fcd6f726e3c8fd03208663aecd388eb
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-04-22 16:44:24 +0000

    End of 2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """Session authentication module for the API.
 """
+from uuid import uuid4
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Session authentication class.
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
